08adf2756c78e2dca301caf45ce0fd987e4f5609
SpreadWater <875737147@qq.com>
2020-10-11 15:06:27 +0800

    [optimize]删除之前测试的注册sp

--- app/src/main/java/com/example/gdmap/ui/viewmodel/LoginOrRegisterViewModel.kt
+++ app/src/main/java/com/example/gdmap/ui/viewmodel/LoginOrRegisterViewModel.kt
@@ -3,6 +3,7 @@ package com.example.gdmap.ui.viewmodel
 import android.annotation.SuppressLint
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.nfc.Tag
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,6 +13,7 @@ import com.example.gdmap.utils.MyApplication
 import com.example.gdmap.utils.Toast
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import kotlin.math.log
 
 /**
  * @Author: xgl
@@ -24,44 +26,31 @@ class LoginOrRegisterViewModel : ViewModel() {
         "remember",
         MODE_PRIVATE
     )
-    private var registerSharedPreferences: SharedPreferences =
-        MyApplication.context.getSharedPreferences("register_account", MODE_PRIVATE)
     val  registerService= ServiceCreator.create(LoginRegister::class.java)
     val loginOrRegisterResult = MutableLiveData<Int>()
     fun login(name: String, password: String) {
+        registerService.SiginIn(name,password)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{
+                if (it.status == 10000) {
+                    rememberPassword(name, password,it.data.token)
+                    loginOrRegisterResult.value = 200
+                    Toast.toast("登录成功")
+                }else
+                    Toast.toast("输入用户名或者是密码有误，请重新输入")
 
-        val savedName = registerSharedPreferences.getString("name", "")
-        val savedPassword = registerSharedPreferences.getString("password", "")
-        if (savedName.equals(name) && savedPassword.equals(password)) {
-            //自动登录
-            rememberPassword(name, password)
-            loginOrRegisterResult.value = 200
-            Toast.toast("登录成功")
-            Log.d("zt","保存的登录")
-        }else{
-            //手动登录
-            registerService.SiginIn(name,password)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{
-                    if (it.status == 10000) {
-                        loginOrRegisterResult.value = 200
-                        Log.d("zt","未保存的登录")
-                        Toast.toast("登录成功")
-                    }else{
-                        Toast.toast("输入用户名或者是密码有误，请重新输入")
-                    }
-                }
         }
     }
 
     @SuppressLint("CommitPrefEdits")
-    private fun rememberPassword(name: String, psw: String) {
+    private fun rememberPassword(name: String, psw: String,token:String) {
         Log.d("tagtag", "$name$psw")
         val editor = rememberPassword.edit()
         editor?.apply {
             putString("name", name)
             putString("psw", psw)
+            putString("token",token)
             apply()
         }
     }
@@ -79,5 +68,8 @@ class LoginOrRegisterViewModel : ViewModel() {
                         Toast.toast("注册失败")
                     }
                 }
+    }
+    companion object{
+       const val TAG="ZT"
     }
 }
